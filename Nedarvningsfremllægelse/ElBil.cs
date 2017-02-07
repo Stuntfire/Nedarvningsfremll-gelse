6d2cd405456bdb0d10b105de4e8f0c86d4e60c14
Søren Roed Schack <soer622t@edu.easj.dk>
2017-02-07 10:36:27 +0100

    Tilføjet en ElBil.cs til vores Solution. ElBil.cs arver fra Bil.cs.

---
+++ Nedarvningsfremllægelse/ElBil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nedarvningsfremllægelse
+{
+    class ElBil : Bil
+    {
+        public ElBil(string mærke, int bilPrisExAfgift, int købsår, int kmPrLiter, string registreringsNr) : base(mærke, bilPrisExAfgift, købsår, kmPrLiter, registreringsNr)
+        {
+        }
+
+        public override int HalvÅrligEjerafgift()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int Rækkevidde()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
